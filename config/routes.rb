fa509f157c8fa5c2ed2246b21d548c3e782b0ba3
Kei <kt10195665@yahoo.ne.jp>
2022-04-11 14:01:08 +0900

    ログインのルーティングとコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  ## ======= 👇 ここから変更する=======
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+  }
+  ## ====== 👆 ここまで変更する=======
 end
